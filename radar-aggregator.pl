c8e99b8064f95fb9f5ee2d8df9380a121f562039
MarxBro <allthemarxbrothers@gmail.com>
2015-11-24 18:56:24 -0300

    hueveando

--- radar-aggregator.pl
+++ radar-aggregator.pl
@@ -23,12 +23,10 @@ use JSON;
 
 use Date::Manip;
 use POSIX q/strftime/;
+use Digest::MD5 qw(md5_hex);
 
- #use Digest::MD5 qw(md5 md5_hex md5_base64);
- use Digest::MD5 qw(md5_hex);
- #$digest = md5($data);
- #$digest = md5_hex($data);
- #$digest = md5_base64($data);
+use Regexp::Common qw(time);
+my $rgx_fecha = $RE{time}{mail};
 
 =pod
 
@@ -48,12 +46,11 @@ my @ENTRIES = ();
 
 
 my $debug = $opts{d} || 0;
-
+my $separador = '='x80 . "\n";
 
 ######################################################################
 # Cod Ppal.
 ######################################################################
-
 
 foreach my $line(@feed_lines){  
   chomp($line);
@@ -73,13 +70,11 @@ my $ENTRIESjson = encode_json \@ENTRIES;
 write_file('data/feeds.json', $FEEDSjson);
 write_file('data/entries.json', $ENTRIESjson);
 
-print Dumper($FEEDSjson);
+#print Dumper($FEEDSjson);
 
-
-
-
-
-
+my $hoy = time();
+my $dia = 60 * 60 * 24;
+my $ayer = $hoy - $dia;
 
 
 ######################################################################
@@ -87,87 +82,69 @@ print Dumper($FEEDSjson);
 ######################################################################
 
 sub get_feed {
-
     # acceder a cada feed.
-
     my $feed_source = shift;
     my $feed   = XML::FeedPP->new($feed_source);
+    #print $separador, Dumper($feed), $separador;
 
-    my @feed_categories = @_;
-
-
-    my $feed_title       = decode_entities($feed->title());
-   
+    my @feed_categories  = @_;
+    my $feed_title       = decode_entities( $feed->title() );
     my $feed_url         = $feed->link();
-    my $feed_url_digest = md5_hex($feed_url);
-    my $feed_description = decode_entities($feed->description());
-
-    my $feed_language	 = $feed->language();
-
+    my $feed_url_digest  = md5_hex($feed_url);
+    my $feed_description = decode_entities( $feed->description() );
+    my $feed_language    = $feed->language();
 
     # entries
-
     my $feeds_counter = 0;
+    my @items_de_hoy = $feed->match_item( pubDate => q/$rgx_fecha/); 
+
 
     foreach my $item ( $feed->get_item() ) {
+        #print $separador, Dumper($item), $separador;
 
         my $item_title = decode_entities($item->title());
 
         #Esto tendria que estar afuera primero.
         my $item_date        = $item->pubDate();
-
         my $item_url         = $item->link();
-        my $item_url_digest = md5_hex($item_url);
-
+        my $item_url_digest  = md5_hex($item_url);
         my $item_description = $item->description();
-        my $item_descriptionClean = HTML::FormatText->new->format(parse_html($item_description));
+        my $item_descriptionClean = HTML::FormatText->new->format( parse_html($item_description) );
         $item_descriptionClean = decode_entities($item_descriptionClean);
-        #item_descriptionClean =~ s/(\.{3})?\s*Read\s*More$//gi;
         $item_descriptionClean =~ s/Read\s*More$//gi;
-
 
         #¿es de las ultimas 24hrs?
         my @item_tags        = $item->category();
         my $item_author      = $item->author();
 
-        my $hoy = time();
-        my $dia = 60 * 60 * 24;
-        my $ayer = $hoy - $dia;
 
         my $x = UnixDate($item_date, "%s"); # cualquier fecha, en timestamp format.
+        say $x;
 
         if($x >= $ayer && $x <= $hoy){
-          say "es de las ultimas 24hrs ($x)";
+          say "es de las ultimas 24hrs ($x)" if $debug;
+            #Guardar en el hash main.
+            push @ENTRIES, {
+                title       => "$item_title",
+                feed        => "$feed_title",
+                feed_url    => "$feed_url",
+                feed_id     => "$feed_url_digest",
+                feed_categories => [@feed_categories],
+                date        => "$item_date",
+                url         => "$item_url",
+                id          => "$item_url_digest",
+                description => "$item_description",
+                descriptionClean => "$item_descriptionClean",
+                tags        => @item_tags,
+                author      => "$item_author"
+            };  
         }else{
-            say "NO es de las ultimas 24hrs ($x)";
+            say "NO es de las ultimas 24hrs ($x)" if $debug;
         }
-
-        #Guardar en el hash main.
-
-        push @ENTRIES, {
-            title       => "$item_title",
-            feed        => "$feed_title",
-            feed_url    => "$feed_url",
-            feed_id     => "$feed_url_digest",
-            feed_categories => [@feed_categories],
-            date        => "$item_date",
-            url         => "$item_url",
-            id          => "$item_url_digest",
-            description => "$item_description",
-            descriptionClean => "$item_descriptionClean",
-            tags        => @item_tags,
-            author      => "$item_author"
-        };  
-
-    	
     	$feeds_counter++;
-
     }
 
-
-
     # feeds
-
     push @FEEDS, {
         title       => "$feed_title",
         url         => "$feed_url",
@@ -178,27 +155,12 @@ sub get_feed {
         n_entries   => "$feeds_counter",
     };
 
-
-
-
     return "### fetched: ", $feed->title() if $debug;
 }
-
-
 
 sub ayudas {
     pod2usage(-verbose=>3);
 }
-
-
-
-
-
-
-
-
-
-
 
 ######################################################################
 __DATA__
