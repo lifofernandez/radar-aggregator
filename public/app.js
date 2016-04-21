ded513e514988bc1b6d8a5c0202cf0c04d1dbf55
Lifo Fernandez <lifofernandez@gmail.com>
2016-04-20 21:11:39 -0300

    agrego ?search

--- public/app.js
+++ public/app.js
@@ -1,17 +1,31 @@
 var App = angular.module('App', []);
 
 App.controller('EntriesCtrl', function($scope, $http) {
+	
+	var search = window.location.search;
+	var entriesJson = 'hoy.json';
+	if(search) {
+		search = search.replace('?','');
+	 	entriesJson = search+'.json';
+		//console.log(search) 
+	}
 
-	$entriesJson = 'hoy.json';
 
-    $http.get($entriesJson)
-        .then(function(res) {
+    $http.get(entriesJson)
+        .then(function successCallback(res) {
+
             angular.forEach(res, function(value, key) {
                 //console.log(res.data);
             });
             $scope.entries = res.data.entries;
-        });
 
+        }, function errorCallback(response) {
+        	document.body.innerHTML = 'no hay nada para > '+ search;
+
+        	//console.log('error');
+	    	// called asynchronously if an error occurs
+	    	// or server returns response with an error status.
+
+	  	});
 });
-
-
+ 
