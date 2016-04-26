959fffaf19c824ca2fb2130a24e0512ab5a38199
MarxBro <allthemarxbrothers@gmail.com>
2016-04-26 20:11:16 -0300

    me convence (sin giladas))

--- p.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use POSIX q/strftime/;
-use feature "say";
-
-my $dia = 60*60*24;
-my $t_banana = strftime ("%d_%B_%Y_%H_%M_%S",localtime(time()-$dia));
-say $t_banana;
