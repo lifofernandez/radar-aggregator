5e0de5f3233e570ef86084160f4650f1caf275ba
Lifo Fernandez <lifofernandez@gmail.com>
2016-05-04 18:58:51 -0300

    rats

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-
+header('Content-Type: charset=utf-8'); 
 /*	Tmpl 	*/
 
 // require_once '../vendor/Twig-1.24.0/lib/Twig/Autoloader.php';
@@ -12,6 +12,7 @@ $twig = new Twig_Environment($loader);
 $twig->addExtension(new Twig_Extensions_Extension_Intl());
 
 $template = $twig->loadTemplate('index.html.twig');
+// $template->set_output_encoding( 'utf-8' );
 
 
 /*	Cargar	*/
