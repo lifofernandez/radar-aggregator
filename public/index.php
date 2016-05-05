5519a8b8527a7e220a053acdd41be461cbceb58b
Lifo Fernandez <lifofernandez@gmail.com>
2016-05-05 19:20:55 -0300

    categorias  y jspin

--- public/index.php
+++ public/index.php
@@ -1,27 +1,49 @@
 <?php
 
-header('Content-Type: charset=utf-8'); 
-/*	Tmpl 	*/
+/*	Imports	*/
 
-// require_once '../vendor/Twig-1.24.0/lib/Twig/Autoloader.php';
+//////////////
+//
+//	Esto va si se isntala de github
+//	require_once '../vendor/Twig-1.24.0/lib/Twig/Autoloader.php';
+// 	Twig_Autoloader::register(); 
+
+//////////////
+//
+//	esto va si: composer require twig/twig:~1.0
 require_once '../vendor/autoload.php';
-// Twig_Autoloader::register(); //esto va si se isntala de github
+
+
+/*	Twig 	*/
 
 $loader = new Twig_Loader_Filesystem('../templates');
 $twig = new Twig_Environment($loader);
 $twig->addExtension(new Twig_Extensions_Extension_Intl());
 
+$categoria = $_GET["categoria"];
+
+
+
+
+
+
 $template = $twig->loadTemplate('index.html.twig');
-// $template->set_output_encoding( 'utf-8' );
 
 
 /*	Cargar	*/
 
 $feedsJson = file_get_contents("../hoy.json");
-//var_dump($feedsJson);
 $feeds = json_decode($feedsJson,true); // 'true' devuelve  array
 
-// var_dump($feeds);
+
+/* Filtro 	*/
+
+$twig->addGlobal('cat', $categoria);
+// var_dump($feeds["timestamp"]);
+
+// foreach ($feeds["timestamp"] as $feed){
+// 	var_dump($feed[0]["feed_categories"]);
+// }
 
 /*	Render	*/
 
